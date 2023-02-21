94342e16d5ceb11cd2d676d3d6221d3ee8ffa1d2
Miguel Ramos <miguito.estudos@gmail.com>
2023-02-21 18:54:30 -0300

    commit inicial do dia-2

---
+++ dia-2/main.js
@@ -0,0 +1,8 @@
+const nome = document.querySelector("#user.nome")
+const idade = document.querySelector("#user.idade")
+const linguagem = document.querySelector("#user.linguagem")
+const botao_confirma = document.querySelector(".confirmar")
+
+botao_confirma.onclick = function(){
+    document.write(`Olá, ${nome.value}, você tem ${idade.value} anos e já está aprendendo ${linguagem.value} !`)
+}
